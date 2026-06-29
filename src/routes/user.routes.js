b503d464d424904d6a4af65dc0f5ee88ca8a17eb
devlopingandroid <yashgoel15119@gmail.com>
2026-06-29 17:48:01 +0530

    Added routers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser,loginUser,logOutUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { registerUser,loginUser,logOutUser,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -23,7 +23,12 @@ const router = Router();
 router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT,logOutUser)
-
 router.route("/refresh-token").post(refreshAccessToken)
-
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").path(verifyJWT,updateAccountDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("/cover-image"),updateCoverImage)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/history").get(verifyJWT,getWatchHistory)
 export default router;
